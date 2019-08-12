512e5ab34b628159172adb83989a75f5731a4037
ben <ben@mulveyfamily.com>
2019-08-11 23:32:30 -0400

    add easier room changes & make list of appliances available/unavailable

--- monitor.py
+++ monitor.py
@@ -1,14 +1,18 @@
 import requests
 import time
 
+room1 = 415890003
+room2 = 415890057
+
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11',
            }
-url1 = "https://www.laundryview.com/api/currentRoomData?school_desc_key=8861&location=415890057"  # smith b
-url2 = "https://www.laundryview.com/api/currentRoomData?school_desc_key=8861&location=415890003"  # smith a
+url1 = "https://www.laundryview.com/api/currentRoomData?school_desc_key=8861&location=%s" % room1 # smith a
+url2 = "https://www.laundryview.com/api/currentRoomData?school_desc_key=8861&location=%s" % room2  # smith b
 
-response_a = requests.get(url2, headers=headers)
-response_b = requests.get(url1, headers=headers)
+response_a = requests.get(url1, headers=headers)
+response_b = requests.get(url2, headers=headers)
 
 if not response_a:  # failed
     time.sleep(30)
@@ -27,21 +31,58 @@ if not response_b:  # failed
 objects_a = response_a.json()['objects']
 objects_b = response_b.json()['objects']
 
-a_washers = {}
-a_dryers = {}
+a_washers = []
+a_dryers = []
 for i in objects_a:
     if i['type'] == "washFL":
-        a_washers.update(i)
+        a_washers.append(i)
     elif i['type'] == "dry":
-        a_dryers.update(i)
+        a_dryers.append(i)
 
-b_washers = {}
-b_dryers = {}
+b_washers = []
+b_dryers = []
 for i in objects_b:
     if i['type'] == "washFL":
-        b_washers.update(i)
+        b_washers.append(i)
     elif i['type'] == "dry":
-        b_dryers.update(i)
+        b_dryers.append(i)
+
+available = []
+unavailable = []
+
+for washer in a_washers:
+    if washer['time_left_lite'] == "Available":
+        available.append({'ID': "%s" % washer["appliance_desc_key"], 'Location': "%s" % washer['appliance_desc'],
+                          'Room': "A"})
+    else:
+        unavailable.append({'ID': "%s" % washer["appliance_desc_key"], 'Location': "%s" % washer['appliance_desc'],
+                           'Room': "A"})
+
+for dryer in a_dryers:
+    if dryer['time_left_lite'] == "Available":
+        available.append({'ID': "%s" % dryer["appliance_desc_key"], 'Location': "%s" % dryer['appliance_desc'],
+                          'Room': "A"})
+    else:
+        unavailable.append({'ID': "%s" % dryer["appliance_desc_key"], 'Location': "%s" % dryer['appliance_desc'],
+                           'Room': "A"})
 
 
+for washer in b_washers:
+    if washer['time_left_lite'] == "Available":
+        available.append({'ID': "%s" % washer["appliance_desc_key"], 'Location': "%s" % washer['appliance_desc'],
+                          'Room': "B"})
+    else:
+        unavailable.append({'ID': "%s" % washer["appliance_desc_key"], 'Location': "%s" % washer['appliance_desc'],
+                           'Room': "B"})
 
+
+for dryer in b_dryers:
+    if dryer['time_left_lite'] == "Available":
+        available.append({'ID': "%s" % dryer["appliance_desc_key"], 'Location': "%s" % dryer['appliance_desc'],
+                          'Room': "B"})
+    else:
+        unavailable.append({'ID': "%s" % dryer["appliance_desc_key"], 'Location': "%s" % dryer['appliance_desc'],
+                           'Room': "B"})
+
+print("Available: " + str(available))
+print("Unavailable: " + str(unavailable))
